6a4f255dbc3c94abd753619b7fc0f6692b871c78
FilipeH14 <filipe.hanielloper@gmail.com>
2022-07-16 01:31:20 -0300

    primeiras configurações de autenticação

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+
+import * as firebase from 'firebase/app'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'front'
+
+  ngOnInit(): void {
+    
+    const firebaseConfig = {
+      apiKey: "AIzaSyC41lkYW2ssl1ThfaJe059MrAH_-9b5wcI",
+      authDomain: "wacthflix.firebaseapp.com",
+      projectId: "wacthflix",
+      storageBucket: "wacthflix.appspot.com",
+      messagingSenderId: "744031875195",
+      appId: "1:744031875195:web:681f770eaeee2cd31e72de"
+    }
+
+    const app = firebase.initializeApp(firebaseConfig)
+  }
 }
--- front/src/app/shared/services/auth.service.ts
+++ front/src/app/shared/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core'
 
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+
 import { User } from '../models/user'
 
 @Injectable({
@@ -9,6 +11,12 @@ export class AuthService {
 
   public toRegisterUser(user: User): void {
     console.log(`Chegamos até o serviço ${user}`)
+
+    const auth = getAuth()
+
+    createUserWithEmailAndPassword(auth, user.email, user.password)
+      .then(resp => console.log(resp))
+      .catch((error: Error) => console.log(error))
   }
 
   constructor() { }
